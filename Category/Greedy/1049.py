7e55ef3132adaddb725d7fd3dce1487aec62e44a
eclat12450@gmail.com <eclat12450@gmail.com>
2022-01-13 03:57:22 +0900

    0113

--- Category/Greedy/1049.py
+++ Category/Greedy/1049.py
@@ -16,6 +16,36 @@ cost = 0
 min_pack = min(pack)
 min_one = min(one)
 
+#크게 3가지 경우의 수가 있음 : 다 팩으로만 사거나 팩으로 사고, 다 낱개로 사거나, 팩으로 채우고, 낱개는 1개짜리로 채우거나
+cost_list = []
+cost_list.append((n // 6 + 1) * min_pack)
+cost_list.append(min_one * n)
+cost_list.append((n // 6) * min_pack + (n % 6) * min_one)
+
+print(min(cost_list))
+
+"""
+if min_pack < min_one: ## 6개짜리 팩이 제일 싼 하나보다도 더 싼 경우에는 그냥 넘치더라도 팩으로만 사는게 이득
+    num_pack = n // 6
+    cost = (num_pack + 1) * min_pack
+elif min_pack/6 == min_one: #팩으로 사는 한개의 가격과 가격이 같은 경우에는 젤싼거 * 6
+    cost = min_one * n
+elif min_pack/6 > min_one: #팩으로 사는 한개의 가격이 하나의 가격보다 비싼 경우에는 다 개당으로 사는게 이득
+    cost = min_one * n
+else: ## 그외의 경우들
+    if (n % 6) * min_one < min_pack: ## 나머지를 낱개로 사는 가격이 한 팩보다 더 싼경우에는 
+        cost = (n // 6) * min_pack + (n % 6) * min_one
+    else: ## 
+        num_pack = n // 6
+        cost = (num_pack + 1) * min_pack
+"""
+
+
+#print(cost)
+
+"""
+
+
 if min_pack < min_one or min_pack/6 < min_one:
     num_pack = n//6
     cost = (num_pack+1)*min_pack
@@ -28,3 +58,4 @@ else:
     print("not find")
 
 print(cost)
+"""
--- Category/Greedy/1715.py
+++ Category/Greedy/1715.py
@@ -1,3 +1,4 @@
+"""
 n = int(input())
 
 array = []
@@ -8,7 +9,45 @@ array.sort()
 
 count = array[0] + array[1]
 count_list = [count]
+save = 0
 i = 2
+
+
+while True:
+    if (i == n-1):
+        count += array[i]
+        count_list.append(count)
+        break
+    elif (i == n-2):
+        if count + array[i] > array[i] + array[i + 1]:
+            count_list.append(array[i] + array[i + 1])
+            count_list.append(count + array[i] + array[i + 1])
+            #count += array[i] + array[i + 1]
+            print('here')
+            break
+        else:
+            count += array[i]
+            count_list.append(count)
+            # print(count_list)
+            count += array[i + 1]
+            count_list.append(count)
+            # print(count_list)
+            break
+    elif count + array[i] > array[i] + array[i + 1]:
+        count_list.append(array[i] + array[i + 1])
+        count += array[i] + array[i + 1]
+        count_list.append(count)
+        i += 2
+    else:
+        count += array[i]
+        count_list.append(count)
+        # print(count_list)
+        i += 1
+
+#print(count_list)
+print(sum(count_list))
+"""
+
 """
 while True:
     if (i == n-1):
@@ -42,4 +81,39 @@ while True:
 
 print(count_list)
 print(sum(count_list))
+
+
+1, 2, 3, 4, 5, 6 의 카드묶음이 있는 경우
+
+
+1,2 = 3
+3,3 = 6
+6,4 = 10
+10,5 = 15
+15, 6 = 21
+
+1,2 = 3
+3,3 = 6
+6,4 = 10
+5, 6 = 11
+10, 11 = 21
+
+
 """
+import heapq # 우선순위 큐
+
+N = int(input())
+
+cardList = list(int(input()) for _ in range(N))
+heapq.heapify(cardList)
+result=0
+
+
+while len(cardList) != 1:
+    num1 = heapq.heappop(cardList)
+    num2 = heapq.heappop(cardList)
+    Sum = num1 + num2
+    result += Sum
+    heapq.heappush(cardList,Sum)
+
+print(result)
