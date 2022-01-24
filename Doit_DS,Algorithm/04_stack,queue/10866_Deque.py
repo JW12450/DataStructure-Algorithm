4865fdc689e1393009820765f9247bf2718767a2
eclat12450@gmail.com <eclat12450@gmail.com>
2022-01-24 18:09:32 +0900

    0124

--- Doit_DS,Algorithm/04_stack,queue/10866_Deque.py
+++ Doit_DS,Algorithm/04_stack,queue/10866_Deque.py
@@ -1,7 +1,8 @@
 import sys
 
-n = int(sys.stdin.readline())\
+n = int(sys.stdin.readline())
 
+"""
 deque = []
 
 for i in range(n):
@@ -38,6 +39,52 @@ for i in range(n):
 
     else:
         if (len(deque) <= 0):
+            print(1)
+        else :
+            print(0)
+"""
+
+from collections import deque
+
+dq = deque()
+for i in range(n):
+    order = sys.stdin.readline().split()
+
+    if order[0] == 'push_front':
+        #deque.insert(0, order[1])
+        dq.appendleft(order[1])
+    elif order[0] == 'push_back':
+        #deque.append(order[1])
+        dq.append(order[1])
+    elif order[0] == 'front':
+        if (len(dq) <= 0):
+            print(-1)
+        else:
+            #print(deque[0])
+            print(dq[0])
+    elif order[0] == 'back':
+        if (len(dq) <= 0):
+            print(-1)
+        else:
+            print(dq[len(dq) -1])
+
+    elif order[0] == 'pop_front':
+        if (len(dq) <= 0):
+            print(-1)
+        else:
+            #print(deque.pop(0))
+            print(dq.popleft())
+    elif order[0] == 'pop_back':
+        if (len(dq) <= 0):
+            print(-1)
+        else:
+            #print(deque.pop())
+            print(dq.pop())
+    elif order[0] == 'size':
+        print(len(dq))
+
+    else:
+        if (len(dq) <= 0):
             print(1)
         else :
             print(0)
--- Doit_DS,Algorithm/05_recursion/11729_하노이탑.py
+++ Doit_DS,Algorithm/05_recursion/11729_하노이탑.py
@@ -3,21 +3,28 @@ import sys
 n = int(sys.stdin.readline())
 
 result = []
-
-def move(count, num, x, y) -> None:
+count = 0
+#원판의 갯수, 시작기둥, 목표기둥
+def move(num, x, y) :
+    global count
+    #가장 바닥에 있는 원반을 제외한 그 위의 그룹들을 시작기둥에서 중간기둥으로 옮김
     if num > 1:
-        count +=1
-        move(count, num-1, x, 6-x-y)
+        move(num-1, x, 6-x-y)
 
+    #가장 바닥의 기둥을 시작기둥에서 목표기둥으로 옮김
+    #print(f'원반 {num} 을 {x}기둥에서 {y}기둥')
+    count += 1
     result.append([x,y])
 
+    #가장 바닥에 있는 원반을 제외한 그 위의 그룹들을 중간기둥에서 목표기둥으로 옮김
     if num > 1:
-        count += 1
-        move(count, num-1, 6-x-y, y)
+        move(num-1, 6-x-y, y)
 
-count = 0
-move(count, n, 1, 3)
-print(len(result))
+
+move(n, 1, 3)
+
+print(count)
+
 for i in range(len(result)):
     for j in range(2):
         print(result[i][j], end=" ")
