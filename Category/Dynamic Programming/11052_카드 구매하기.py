58c114228032657f1a36bda040f04fd71d41bcd5
] <eclat12450@gmail.com>
2022-02-22 00:01:37 +0900

    0222

--- Category/Dynamic Programming/11052_카드 구매하기.py
+++ Category/Dynamic Programming/11052_카드 구매하기.py
@@ -1,4 +1,17 @@
 n = int(input())
 card = list(map(int, input().split()))
+card.insert(0,0)
 
-print(card)
+dp = [0]*(n+1)
+dp[1] = card[1]
+
+for i in range(2, n+1):
+    cand = []
+    for j in range(1, i+1):
+        cand.append(dp[i-j]+card[j])
+    dp[i] = max(cand)
+#dp[2] = max(dp[1]+card[1], card[2])
+#dp[3] = max(dp[2]+card[1], dp[1]+card[2], dp[0]+card[3])
+#dp[4] = max(dp[3]+card[1], dp[2]+card[2], dp[1]+card[3], dp[0]+card[4])
+
+print(dp[n])
