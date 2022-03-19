6b39e3243eb60edec6887503718d0ddf60cdd3da
JW12450 <eclat12450@gmail.com>
2022-03-19 13:23:44 +0900

    0319

--- Category/Dynamic Programming/1915_가장 큰 정사각형.py
+++ Category/Dynamic Programming/1915_가장 큰 정사각형.py
@@ -1,47 +1,20 @@
-import sys
-from collections import deque
+n, m = map(int, input().split())
 
+data = []
 
-def getMaxField(data, n, m):
-    '''
-    n개의 줄에 m개의 숫자가 차례로 주어질 때, 가장 큰 잡초밭을 출력하는 함수를 작성하세요.
-    '''
-    ans = []
-    for i in range(n):
-        for j in range(m):
-            square = True
-            if data[i][j] == 0:
-                continue
-            else:
-                width = 0
-                while j + width < n and i + width < m and data[i][j + width]:
-                    width += 1
-                for x in range(i, i + width):
-                    for y in range(j, j + width):
-                        if data[x][y] != 1:
-                            square = False
-                if square:
-                    ans.append(width * width)
-                # print(ans)
+for i in range(n):
+    s = input()
+    data.append(list(map(int, list(s))))
 
-    return ans
+dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+side = 0
 
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if data[i - 1][j - 1] == 1:
+            dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
 
-def main():
-    '''
-    이 부분은 수정하지 마세요.
-    '''
-    inputList = [int(x) for x in input().split()]
+            if dp[i][j] > side:
+                side = dp[i][j]
 
-    n = inputList[0]
-    m = inputList[1]
-    #data = [[int(x) for x in input().split()] for i in range(n)]
-    data = [list(map(int, list(input()))) for i in range(n)]
-    for d in data:
-        print(d)
-    print(max(getMaxField(data, n, m)))
-
-
-if __name__ == "__main__":
-    main()
-
+print(side ** 2)
