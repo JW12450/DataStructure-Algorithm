0f55a09f233f6049c7be3aabf095801a32584760
jinwoo <eclat12450@gmail.com>
2022-04-01 19:58:29 +0900

    0401

---
+++ Category/Greedy/15553_난로.py
@@ -0,0 +1,18 @@
+import sys, heapq
+n , k = map(int, sys.stdin.readline().split())
+
+friend = []
+for _ in range(n):
+    friend.append(int(sys.stdin.readline()))
+
+q = []
+for i in range(1,n):
+    heapq.heappush(q, friend[i]- friend[i-1])
+
+ans = 1
+#n-k번동안은 차이값만큼을 그래도 켜줘야 이어짐
+for i in range(n-k):
+    ans += heapq.heappop(q)
+
+ans += len(q)
+print(ans)
